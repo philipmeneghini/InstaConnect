54a6eafe6328b69ca0dff58543b06e31cdc954a5
Philip Meneghini <phil.meneghini@yahoo.com>
2023-04-25 22:53:45 -0400

    changed auth endpoint route

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -8,7 +8,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
