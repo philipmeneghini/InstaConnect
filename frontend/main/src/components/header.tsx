879446d1995897a7299e38de15eebcf7dfc372a8
Philip Meneghini <phil.meneghini@yahoo.com>
2023-08-11 20:16:56 -0400

    header navbar created with avatar and menu button

--- frontend/main/src/components/header.tsx
+++ frontend/main/src/components/header.tsx
@@ -1,15 +1,73 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import { AppBar, Container, Grid, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
 import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
 
 export const Header = () => {
+
+    const [anchorUser, setAnchorUser] = useState<HTMLElement | null>(null)
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorUser(event.currentTarget)
+    }
+
+    const handleCloseUserMenu = () => {
+        setAnchorUser(null)
+    }
+
     return (
         <AppBar position='static'>
             <Container maxWidth='xl'>
                 <Toolbar disableGutters>
-                <Typography variant='h4' component='div' sx={{ flexGrow: 1 }}>
-                    InstaConnect
-                </Typography>
-                <Avatar/>
+                    <Grid container xs={12}>
+                        <Grid item xs = {5} sx={{ flexGrow: 1, display: 'flex', justifyContent: 'start' }}>
+                            <IconButton color='inherit'>
+                                <MenuIcon />
+                            </IconButton>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <IconButton color='inherit'>
+                                <Typography variant='h4'>
+                                        InstaConnect
+                                </Typography>
+                            </IconButton>
+                        </Grid>
+                        <Grid item xs={5} sx={{ flexGrow: 0, display: 'flex', justifyContent: 'end'}}>
+                            <Tooltip title='Open Settings'>
+                                <IconButton onClick={handleOpenUserMenu}>
+                                    <Avatar alt='Remy Sharp'/>
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                            sx={{ mt: '45px' }}
+                            id='menu-appbar'
+                            anchorEl={anchorUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorUser)}
+                            onClose={handleCloseUserMenu}
+                            >
+                                <MenuItem key='Profile' onClick={handleCloseUserMenu}>
+                                    <Typography textAlign='center'> Profile </Typography>
+                                </MenuItem>
+                                <MenuItem key='Account' onClick={handleCloseUserMenu}>
+                                    <Typography textAlign='center'> Account </Typography>
+                                </MenuItem>
+                                <MenuItem key='Logout' onClick={handleCloseUserMenu}>
+                                    <Typography textAlign='center'> Logout </Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </Container>
         </AppBar>
